b8a1d5ba159599e6fddf3f2bfb55eeff959b17b3
scalespeeder <66777640+scalespeeder@users.noreply.github.com>
2023-08-30 15:34:20 +0100

    Add files via upload

---
+++ tradership.c
@@ -0,0 +1,63 @@
+// Essex Reapers Trader Ship
+// Trader Ship
+SpawnObject( "Land_Ship_Big_FrontA", "6888.49 12 2600", "90 0 0" );
+SpawnObject( "Land_Ship_Big_FrontB", "6886.18 10.5 2619.9", "90 0 0" );
+SpawnObject( "Land_Ship_Big_BackA", "6884.95 12 2655.4", "90 0 0" );
+SpawnObject( "Land_Ship_Big_BackB", "6885.5 10.5 2675", "90 0 0" );
+SpawnObject( "Land_Ship_Big_Castle", "6885.3 20.65 2663.6", "90 0 0" );
+SpawnObject( "Bonfire", "6885 21 2670", "0 0 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "6885.3 -7.55 2641.5", "0 10 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "6885.3 -7.55 2623.5", "0 10 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "6885.3 -7.55 2597.6", "0 10 0" );
+SpawnObject( "Land_Pier_Crane_A", "6866.5 8.3 2574", "0 0 0" );
+SpawnObject( "Land_Pier_Crane_b", "6869.5 21.2 2578.2", "-45 0 0" );
+SpawnObject( "Land_Pier_Crane_A", "6867.5 8.3 2706", "0 0 0" );
+SpawnObject( "Land_Pier_Crane_b", "6872.2 21.2 2703", "30 0 0" );
+SpawnObject( "Land_Container_1Aoh", "6877.9 11.8 2587", "22 0 0" );
+SpawnObject( "Land_Container_1Bo", "6867 4.1 2585", "12 0 0" );
+SpawnObject( "Land_Castle_Stairs", "6867.1 0.65 2648.5", "0 0 0" );
+SpawnObject( "StaticObj_Wall_CncBarrier_Block", "6876.6 6.6 2649.5", "90 0 180" );
+SpawnObject( "Land_Roadblock_Table", "6878.4 7.9 2643.6", "45 0 0" );
+SpawnObject( "Land_Roadblock_Table", "6886 7.9 2643.4", "0 0 0" );
+SpawnObject( "Land_Roadblock_Table", "6886 7.9 2653.4", "0 0 0" );
+SpawnObject( "Land_Roadblock_Table", "6891.7 7.9 2652.6", "45 0 0" );
+SpawnObject( "Land_Roadblock_Table", "6891.6 7.9 2644", "-45 0 0" );
+SpawnObject( "Land_Roadblock_Table", "6878.4 7.9 2653", "-45 0 0" );
+SpawnObject( "Land_Mil_GuardShed", "6836.5 3.5 2659.1", "-90 0 0" );
+SpawnObject( "StaticObj_Wall_FenR_3", "6822.4 4.2 2662.5", "65 0 0" );
+SpawnObject( "StaticObj_Wall_FenR_3", "6824.3 4.2 2660", "-160 0 0" );
+// Drydock
+SpawnObject( "Land_Ship_Big_FrontA", "7137 24.4 2614", "143 0 0" );
+SpawnObject( "Land_Ship_Big_FrontB", "7151.5 22.9 2627.8", "143 0 0" );
+SpawnObject( "Land_Ship_Big_BackA", "7178.9 24.4 2650", "143 0 0" );
+SpawnObject( "Land_Ship_Big_BackB", "7194.9 22.8 2661.3", "143 0 0" );
+SpawnObject( "Land_Ship_Big_Castle", "7185.7 33.05 2654.6", "143 0 0" );
+SpawnObject( "Land_Misc_Scaffolding", "7166 8.6 2655.41", "53 0 0" );
+SpawnObject( "Land_Misc_Scaffolding", "7136.5 8.6 2632", "48 0 0" );
+// Flats
+SpawnObject( "Land_Tenement_big", "6779.5 26 2513.2", "0 0 0" );
+SpawnObject( "StaticObj_Garbage_Pile4", "6787.5 3.1 2503.1", "10 0 0" );
+SpawnObject( "StaticObj_Platform2_Stairs_30", "6779 3.9 2525", "180 0 0" );
+SpawnObject( "StaticObj_Platform2_Stairs_30_WallR", "6780.25 3.9 2524.1", "180 0 0" );
+SpawnObject( "StaticObj_Platform2_Stairs_30_WallL", "6777.8 3.9 2524.1", "180 0 0" );
+SpawnObject( "StaticObj_Road_Sidewalk_Stairs", "6778.27 7.06 2516.9", "0 0 0" );
+// Ship Near
+SpawnObject( "Land_Ship_Big_FrontA", "7059 13 2207", "0 0 0" );
+SpawnObject( "Land_Ship_Big_FrontB", "7039.1 11.5 2204.65", "0 0 0" );
+SpawnObject( "Land_Ship_Big_BackA", "7005 13 2203.4", "0 0 0" );
+SpawnObject( "Land_Ship_Big_BackB", "6985.3 11.5 2204", "0 0 0" );
+SpawnObject( "Land_Ship_Big_Castle", "6996.7 21.65 2203.75", "0 0 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "7027.1 -6.42 2204", "90 10 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "7019 -6.42 2204", "-90 10 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "7061.3 -6.42 2204", "-90 10 0" );
+SpawnObject( "Bonfire", "6990.5 21.5 2204", "0 0 0" );
+// Ship Far
+SpawnObject( "Land_Ship_Big_FrontA", "7859 13 2207", "0 0 0" );
+SpawnObject( "Land_Ship_Big_FrontB", "7839.1 11.5 2204.65", "0 0 0" );
+SpawnObject( "Land_Ship_Big_BackA", "7805 13 2203.4", "0 0 0" );
+SpawnObject( "Land_Ship_Big_BackB", "7785.3 11.5 2204", "0 0 0" );
+SpawnObject( "Land_Ship_Big_Castle", "7796.7 21.65 2203.75", "0 0 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "7827.1 -6.42 2204", "90 10 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "7819 -6.42 2204", "-90 10 0" );
+SpawnObject( "StaticObj_Tisy_RadarPlatform_Mid", "7861.3 -6.42 2204", "-90 10 0" );
+SpawnObject( "Bonfire", "7790.5 21.5 2204", "0 0 0" );
